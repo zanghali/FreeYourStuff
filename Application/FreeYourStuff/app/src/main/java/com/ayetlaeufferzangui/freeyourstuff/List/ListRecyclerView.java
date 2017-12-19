db2d6665ea4e6f2ef93def91504282448c0d8d5b
aurelien ayet <Ayet>
2017-12-19 09:36:37 +0100

    Branch Application 50 Creation of the project

---
+++ Application/FreeYourStuff/app/src/main/java/com/ayetlaeufferzangui/freeyourstuff/List/ListRecyclerView.java
@@ -0,0 +1,14 @@
+package com.ayetlaeufferzangui.freeyourstuff.List;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ayetlaeufferzangui.freeyourstuff.R;
+
+public class ListRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_recycler_view);
+    }
+}
---
+++ Application/FreeYourStuff/app/src/main/java/com/ayetlaeufferzangui/freeyourstuff/Navigation/NavigationActivity.java
@@ -0,0 +1,15 @@
+package com.ayetlaeufferzangui.freeyourstuff.Navigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ayetlaeufferzangui.freeyourstuff.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
