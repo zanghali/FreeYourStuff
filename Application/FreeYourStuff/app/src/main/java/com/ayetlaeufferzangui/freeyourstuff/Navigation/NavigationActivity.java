f557a760aa1a1f6ba5560a6f1a61a50ab6ec2c38
Lothaire Laeuffer <lothaire.laeuffer@gmail.com>
2017-12-19 16:27:14 +0100

    #12 create item list

--- Application/FreeYourStuff/app/src/main/java/com/ayetlaeufferzangui/freeyourstuff/Navigation/NavigationActivity.java
+++ Application/FreeYourStuff/app/src/main/java/com/ayetlaeufferzangui/freeyourstuff/Navigation/NavigationActivity.java
@@ -1,15 +1,25 @@
 package com.ayetlaeufferzangui.freeyourstuff.Navigation;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ayetlaeufferzangui.freeyourstuff.List.ListFragment;
 import com.ayetlaeufferzangui.freeyourstuff.R;
 
 public class NavigationActivity extends AppCompatActivity {
+
+    ListFragment mListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        mListFragment = new ListFragment();
+
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().add(R.id.content, mListFragment).commit();
+
     }
 }
