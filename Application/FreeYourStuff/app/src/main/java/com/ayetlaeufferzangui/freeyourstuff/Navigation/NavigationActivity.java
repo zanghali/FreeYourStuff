9d4a474f010b717826695df77ec8a7babed408bd
Lothaire Laeuffer <lothaire.laeuffer@gmail.com>
2017-12-21 15:17:19 +0100

    implement swiping between Fragment

--- Application/FreeYourStuff/app/src/main/java/com/ayetlaeufferzangui/freeyourstuff/Navigation/NavigationActivity.java
+++ Application/FreeYourStuff/app/src/main/java/com/ayetlaeufferzangui/freeyourstuff/Navigation/NavigationActivity.java
@@ -1,28 +1,43 @@
 package com.ayetlaeufferzangui.freeyourstuff.Navigation;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 
-import com.ayetlaeufferzangui.freeyourstuff.List.ListFragment;
+
 import com.ayetlaeufferzangui.freeyourstuff.R;
 
-public class NavigationActivity extends AppCompatActivity {
+public class NavigationActivity extends FragmentActivity {
 
-    ListFragment mListFragment;
+
+    public static PagerAdapter mPagerAdapter;
+    private ViewPager vPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
+        // Instantiate a ViewPager and a PagerAdapter.
+        vPager = (ViewPager) findViewById(R.id.viewpager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        vPager.setAdapter(mPagerAdapter);
 
-        if (savedInstanceState == null){
-            mListFragment = new ListFragment();
-            FragmentManager manager = getSupportFragmentManager();
-            manager.beginTransaction().add(R.id.content, mListFragment).commit();
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (vPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            vPager.setCurrentItem(vPager.getCurrentItem() - 1);
         }
-
-
     }
 }
