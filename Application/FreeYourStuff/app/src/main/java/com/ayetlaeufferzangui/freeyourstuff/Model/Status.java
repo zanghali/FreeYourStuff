400261984b8dfddcef3993cd24f9fb12c106a263
Lothaire Laeuffer <lothaire.laeuffer@gmail.com>
2018-01-08 20:15:24 +0100

    #15 createItem

---
+++ Application/FreeYourStuff/app/src/main/java/com/ayetlaeufferzangui/freeyourstuff/Model/Status.java
@@ -0,0 +1,13 @@
+package com.ayetlaeufferzangui.freeyourstuff.Model;
+
+/**
+ * Created by lothairelaeuffer on 08/01/2018.
+ */
+
+public enum Status {
+    waiting,
+    inProgress,
+    done,
+    aborted
+
+}
