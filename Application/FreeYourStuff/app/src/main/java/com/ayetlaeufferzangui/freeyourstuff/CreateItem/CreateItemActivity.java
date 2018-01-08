71f35578beaa613f6150d94972639245327cd6e6
Lothaire Laeuffer <lothaire.laeuffer@gmail.com>
2018-01-08 23:54:59 +0100

    #15 user cannot submit if a require field is not fill

--- Application/FreeYourStuff/app/src/main/java/com/ayetlaeufferzangui/freeyourstuff/CreateItem/CreateItemActivity.java
+++ Application/FreeYourStuff/app/src/main/java/com/ayetlaeufferzangui/freeyourstuff/CreateItem/CreateItemActivity.java
@@ -3,8 +3,12 @@ package com.ayetlaeufferzangui.freeyourstuff.CreateItem;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -31,9 +35,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 
-public class CreateItemActivity extends AppCompatActivity {
+public class CreateItemActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button mButton;
+
     MaterialBetterSpinner mCategory;
     TextInputEditText mTitle;
     TextInputEditText mDescription;
@@ -42,17 +47,83 @@ public class CreateItemActivity extends AppCompatActivity {
     TextInputEditText mPhone;
     MaterialBetterSpinner mAvailability;
 
+    TextInputLayout mTitleLayout;
+    TextInputLayout mDescriptionLayout;
+    TextInputLayout mPhotoLayout;
+    TextInputLayout mAddressLayout;
+    TextInputLayout mPhoneLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_item);
 
+        mButton = (Button)findViewById(R.id.button);
+        mCategory = (MaterialBetterSpinner)findViewById(R.id.category_spinner);
+        mTitle = (TextInputEditText)findViewById(R.id.title);
+        mDescription = (TextInputEditText)findViewById(R.id.description);
+        mPhoto = (TextInputEditText)findViewById(R.id.photo);
+        mAddress = (TextInputEditText)findViewById(R.id.address);
+        mPhone = (TextInputEditText)findViewById(R.id.phone);
+        mAvailability = (MaterialBetterSpinner)findViewById(R.id.availability_spinner);
+
+        mTitleLayout = (TextInputLayout) findViewById(R.id.title_layout);
+        mDescriptionLayout = (TextInputLayout) findViewById(R.id.description_layout);
+        mPhotoLayout = (TextInputLayout) findViewById(R.id.photo_layout);
+        mAddressLayout = (TextInputLayout) findViewById(R.id.address_layout);
+        mPhoneLayout = (TextInputLayout) findViewById(R.id.phone_layout);
+
+        initSelectFields();
+
+        mButton.setOnClickListener(this);
+
+        removeErrorFromInput();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        boolean i = true;
+        if( TextUtils.isEmpty(mCategory.getText())){
+            mCategory.setError( getResources().getString(R.string.category) + " " + getResources().getString(R.string.required) );
+            i = false;
+        }
+        if( TextUtils.isEmpty(mTitle.getText())) {
+            mTitleLayout.setError( getResources().getString(R.string.title) + " " + getResources().getString(R.string.required) );
+            i = false;
+        }
+        if( TextUtils.isEmpty(mDescription.getText())) {
+            mDescriptionLayout.setError( getResources().getString(R.string.description) + " " + getResources().getString(R.string.required) );
+            i = false;
+        }
+        if( TextUtils.isEmpty(mPhoto.getText())) {
+            mPhotoLayout.setError( getResources().getString(R.string.photo) + " " + getResources().getString(R.string.required) );
+            i = false;
+        }
+        if( TextUtils.isEmpty(mAddress.getText())) {
+            mAddressLayout.setError( getResources().getString(R.string.address) + " " + getResources().getString(R.string.required) );
+            i = false;
+        }
+        if( TextUtils.isEmpty(mAvailability.getText())){
+            mAvailability.setError( getResources().getString(R.string.availability) + " " + getResources().getString(R.string.required) );
+            i = false;
+        }
+
+        if(i == true){
+            //send data to server
+            new HttpAsyncTask().execute();
+        }
+    }
+
+
+    private void initSelectFields(){
         // CATEGORY SELECT
         ArrayAdapter<Category> adapterCategory = new ArrayAdapter<Category>(this,
-                    android.R.layout.simple_dropdown_item_1line,
-                    Category.values()
-            );
+                android.R.layout.simple_dropdown_item_1line,
+                Category.values()
+        );
         MaterialBetterSpinner materialDesignSpinnerCategory = (MaterialBetterSpinner)
                 findViewById(R.id.category_spinner);
         materialDesignSpinnerCategory.setAdapter(adapterCategory);
@@ -65,29 +136,75 @@ public class CreateItemActivity extends AppCompatActivity {
         MaterialBetterSpinner materialDesignSpinnerAvailability = (MaterialBetterSpinner)
                 findViewById(R.id.availability_spinner);
         materialDesignSpinnerAvailability.setAdapter(adapterAvailability);
+    }
 
 
-        mButton = (Button)findViewById(R.id.button);
-        mCategory = (MaterialBetterSpinner)findViewById(R.id.category_spinner);
-        mTitle = (TextInputEditText)findViewById(R.id.title);
-        mDescription = (TextInputEditText)findViewById(R.id.description);
-        mPhoto = (TextInputEditText)findViewById(R.id.photo);
-        mAddress = (TextInputEditText)findViewById(R.id.address);
-        mPhone = (TextInputEditText)findViewById(R.id.phone);
-        mAvailability = (MaterialBetterSpinner)findViewById(R.id.availability_spinner);
+    private void removeErrorFromInput(){
+        mTitle.addTextChangedListener( new TextWatcher() {
 
+            public void afterTextChanged(Editable s) {}
 
-        mButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                //send data to server
-                new HttpAsyncTask().execute();
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                mTitleLayout.setErrorEnabled( false );
             }
         });
+        mDescription.addTextChangedListener( new TextWatcher() {
 
+            public void afterTextChanged(Editable s) {}
 
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
 
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                mDescriptionLayout.setErrorEnabled( false );
+            }
+        });
+        mPhoto.addTextChangedListener( new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                mPhotoLayout.setErrorEnabled( false );
+            }
+        });
+        mAddress.addTextChangedListener( new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                mAddressLayout.setErrorEnabled( false );
+            }
+        });
+        mPhone.addTextChangedListener( new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                mPhoneLayout.setErrorEnabled( false );
+            }
+        });
     }
 
 
